b3143188931d1bea1a872e6161d00e995d8f5f70
Zachary Waldowski <zach@waldowski.me>
2016-08-15 20:42:49 -0400

    WIP: Include AtomicSwift directly, use id-as-Any for cleanup

--- Sources/Deferred/MemoStore.swift
+++ Sources/Deferred/MemoStore.swift
@@ -7,83 +7,65 @@
 //
 
 import Dispatch
-#if SWIFT_PACKAGE
-import AtomicSwift
-#endif
-
-// Atomic compare-and-swap, but safe for an initialize-once, owning pointer:
-//  - ObjC: "MyObject *__strong *"
-//  - Swift: "UnsafeMutablePointer<MyObject!>"
-// If the assignment is made, the new value is retained by its owning pointer.
-// If the assignment is not made, the new value is not retained.
-private func atomicInitialize<T: AnyObject>(_ target: UnsafeMutablePointer<T?>, to desired: T) -> Bool {
-    let newPtr = Unmanaged.passRetained(desired).toOpaque()
-    let wonRace = target.withMemoryRebound(to: Optional<UnsafeMutableRawPointer>.self, capacity: 1) {
-        OSAtomicCompareAndSwapPtr(nil, newPtr, $0)
-    }
-    if !wonRace {
-        Unmanaged.passUnretained(desired).release()
-    }
-    return wonRace
-}
-
-// In order to assign the value of a scalar in a Deferred using atomics, we must
-// box it up into something word-sized. See `atomicInitialize` above.
-private final class Box<T> {
-    let contents: T
-    
-    init(_ contents: T) {
-        self.contents = contents
-    }
-}
+import Atomics
 
 // Heap storage that is initialized with a value once-and-only-once, atomically.
-final class MemoStore<Value> {
-    // Using `ManagedBufferPointer` has advantages over a custom class:
-    //  - The data is efficiently stored in tail-allocated buffer space.
+final class MemoStore<Value>: ManagedBuffer<Void, AnyObject?> {
+
+    // Using `ManagedBuffer` has advantages over a custom class:
     //  - The buffer has a stable pointer when locked to a single element.
     //  - Better `holdsUniqueReference` support allows for future optimization.
-    private typealias Manager = ManagedBufferPointer<Void, Element>
-    private typealias Element = Box<Value>?
 
     static func create(with value: Value?) -> MemoStore<Value> {
-        // Create storage. Swift uses a two-stage system.
-        let ptr = Manager(bufferClass: self, minimumCapacity: 1) { (buffer, _) in
-            // Assign the initial value to managed storage
-            Manager(unsafeBufferObject: buffer).withUnsafeMutablePointers { (_, boxPtr) in
-                boxPtr.initialize(to: value.map(Box.init))
+        return unsafeDowncast(create(minimumCapacity: 1, makingHeaderWith: { (buffer) -> Void in
+            buffer.withUnsafeMutablePointerToElements { (boxPtr) in
+                boxPtr.initialize(to: value.map { $0 as AnyObject })
             }
-        }
-        
-        // Kindly give back an instance of the ManagedBufferPointer's buffer - self.
-        return unsafeDowncast(ptr.buffer, to: MemoStore<Value>.self)
+        }), to: self)
     }
 
-    private init() {
-        fatalError("Unavailable method cannot be called")
-    }
-
-    private func withUnsafeMutablePointer<Return>(_ body: (UnsafeMutablePointer<Element>) -> Return) -> Return {
-        return Manager(unsafeBufferObject: self).withUnsafeMutablePointers { body($1) }
-    }
-    
     deinit {
-        // UnsafeMutablePointer.destroy() is faster than destroy(_:) for single elements
-        _ = withUnsafeMutablePointer { boxPtr in
+        _ = withUnsafeMutablePointerToElements { (boxPtr) in
             boxPtr.deinitialize()
         }
     }
     
     func withValue(_ body: (Value) -> Void) {
-        withUnsafeMutablePointer { boxPtr in
-            guard let box = boxPtr.pointee else { return }
-            body(box.contents)
+        withUnsafeMutablePointerToElements { target in
+            guard let ptr = target.withMemoryRebound(to: UnsafeAtomicRawPointer.self, capacity: 1, {
+                $0.pointee.load(order: .relaxed)
+            }), let unboxed = Unmanaged<AnyObject>.fromOpaque(ptr).takeUnretainedValue() as? Value else { return }
+
+            body(unboxed)
         }
     }
     
+    // Atomic compare-and-swap, but safe for an initialize-once, owning pointer:
+    //  - ObjC: "MyObject *__strong *"
+    //  - Swift: "UnsafeMutablePointer<MyObject!>"
+    // If the assignment is made, the new value is retained by its owning pointer.
+    // If the assignment is not made, the new value is not retained.
     func fill(_ value: Value) -> Bool {
-        return withUnsafeMutablePointer { boxPtr in
-            atomicInitialize(boxPtr, to: Box(value))
+        let newPtr = Unmanaged.passRetained(value as AnyObject).toOpaque()
+
+        let wonRace = withUnsafeMutablePointerToElements { target in
+            target.withMemoryRebound(to: UnsafeAtomicRawPointer.self, capacity: 1) {
+                $0.pointee.compareAndSwap(from: nil, to: newPtr, success: .sequentiallyConsistent, failure: .sequentiallyConsistent)
+            }
+        }
+
+        if !wonRace {
+            Unmanaged<AnyObject>.fromOpaque(newPtr).release()
+        }
+
+        return wonRace
+    }
+
+    var isFilled: Bool {
+        return withUnsafeMutablePointerToElements { target in
+            target.withMemoryRebound(to: UnsafeAtomicRawPointer.self, capacity: 1, {
+                $0.pointee.load(order: .relaxed)
+            }) != nil
         }
     }
 }
